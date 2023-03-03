d367e90aa419e8506b028a7faf18c14ec9575a5f
NEO400 <120325099+NEO400@users.noreply.github.com>
2023-03-03 18:40:40 +0200

    Add main hangman, game, art and words file.

--- Day7/main.py
+++ Day7/main.py
@@ -1,17 +1,57 @@
-#Step 1 
-Word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Import the necessary modules
 import random
-chosen_word = random.choice(Word_list)
+from hangman_words import word_list
+from hangman_art import logo, stages
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-Guess = input("Guess a Letter:").lower()
+# Select a random word from the list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-  if letter == Guess:
-    print("Right")
-  else:
-    print("Wrong")
-    
+# Initialize the game
+end_of_game = False
+lives = 6
+
+# Display the game logo
+print(logo)
+
+# Display the solution (for testing purposes only)
+print(f'Pssst, the solution is {chosen_word}.')
+
+# Initialize the display
+display = []
+for _ in range(word_length):
+    display += "_"
+
+# Loop until the end of the game
+while not end_of_game:
+    # Ask the user to make a guess
+    guess = input("Guess a letter: ").lower()
+
+    # Check if the user has already guessed the letter
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    # Update the display with the guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    # If the guessed letter is not in the word, decrement the lives
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Display the current status of the word
+    print(f"{' '.join(display)}")
+
+    # If the word has been fully guessed, end the game and declare victory
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    # Display the current status of the hangman
+    print(stages[lives])
